d790626d9f9d960ff44cba8e587b4960aacb1527
David Pallarés <serallapdivad@gmail.com>
2022-02-12 21:21:51 +0000

    -Manager can create and delete jobOffer -Manager can update a job offer for -Manager can get ONE job offer -Manager can get ALL job offers

--- api/controllers/jobOffer.controller.js
+++ api/controllers/jobOffer.controller.js
@@ -19,22 +19,56 @@ async function deleteJobOffer(req, res) {
     );
     res.status(200).json(`${jobOffer.title} has been delete`);
   } catch (error) {
-    res.status(500).send(`Error posting job offer: ${error}`);
+    res.status(500).send(`Error deleting job offer: ${error}`);
     throw new Error(`Error deleting job offer: ${error}`);
   }
 }
 
-async function getJobOffer(req, res) {
+async function getAllJobOffers(req, res) {
   try {
+    const jodOffers = await JobOfferModel.find(req.query);
+    res.status(200).json(jodOffers);
   } catch (error) {
-    res.status(500).send(`Error posting job offer: ${error}`);
-    throw new Error(`Error posting job offer: ${error}`);
+    res.status(500).send(`Error obtaining job offers: ${error}`);
+    throw new Error(`Error obtaining job offers: ${error}`);
   }
+}
+
+async function getOneJobOffer(req, res) {
+  try {
+    const jobOffer = await JobOfferModel.findById(req.params.jobOfferId);
+    res.status(200).json({
+      Title: jobOffer.title,
+      Company: jobOffer.company,
+      Description: jobOffer.description,
+      Work_model: jobOffer.workModel,
+      Location: jobOffer.location,
+      Salary: jobOffer.salary,
+    });
+  } catch (error) {
+    res.status(500).send(`Error obtaining job offer: ${error}`);
+    throw new Error(`Error obtaining job offer: ${error}`);
+  }
+}
+
+async function updateJobOffer(req, res) {
+  try {
+    const jobOffer = await JobOfferModel.findByIdAndUpdate(
+      req.params.jobOfferId, 
+      req.body,
+      {
+        new: true,
+        runValidator: true
+      }
+    );
+    res.status(200).json(`${jobOffer.title} post has been successfully updated`)
+  } catch (error) {}
 }
 
 module.exports = {
   createJobOffer,
   deleteJobOffer,
-  //   getAllJobOffers,
-  //   getOneJobOffer,
+  getAllJobOffers,
+  getOneJobOffer,
+  updateJobOffer,
 };
--- api/routes/jobOffer.router.js
+++ api/routes/jobOffer.router.js
@@ -1,14 +1,18 @@
 const router = require("express").Router();
 const { authUser, authAdmin, authManager, authRole } = require("../utils"); // Authenticated Route
 
-const { 
-    createJobOffer,
-    deleteJobOffer,
-    // getAllJobOffers,
-    // getOneJobOffer
- } = require("../controllers/jobOffer.controller");
+const {
+  createJobOffer,
+  deleteJobOffer,
+  getAllJobOffers,
+  getOneJobOffer,
+  updateJobOffer
+} = require("../controllers/jobOffer.controller");
 
 router.post("/", authUser, authManager, createJobOffer);
 router.delete("/:jobOfferId", authUser, authManager, deleteJobOffer);
+router.get("/", authUser, authManager, getAllJobOffers);
+router.get("/:jobOfferId", authUser, authManager, getOneJobOffer);
+router.put("/:jobOfferId", authUser, authManager, updateJobOffer);
 
 module.exports = router;
