c8bc500545543c82b5d8b2a33977be725c39883b
David Pallarés <serallapdivad@gmail.com>
2022-02-19 01:21:25 +0000

    revert populate

--- api/controllers/profile.controller.js
+++ api/controllers/profile.controller.js
@@ -16,13 +16,7 @@ async function getUserProfile(req, res) {
   try {
     const user = await UserModel.findById(res.locals.user.id)
       .populate("experience")
-      .populate({
-        path: "requisition",
-        populate: {
-          path: "jobPost",
-          model: "jobOffer",
-        },
-      });
+      .populate({"requisition"});
     res.status(200).json(user);
   } catch (error) {
     res.status(500).send(`Error getting user profile: ${error}`);
